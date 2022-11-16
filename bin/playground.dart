c6e8860651a41268afa56d0ecd50e02d7ea08bfb
FWP <fred.pereira@rgtitecnologia.com.br>
2022-11-16 14:28:17 -0300

    add operador DotEnv

--- bin/playground.dart
+++ bin/playground.dart
@@ -2,7 +2,6 @@ import 'package:playground/dotenv.dart';
 
 void main() async {
   DotEnv dotEnv = DotEnv();
-
   print(dotEnv['DATABASE_URL']);
   print(dotEnv['SWAGGER']);
   print(dotEnv['JWT_ACCESS_TOKEN_EXPIRESIN']);
--- lib/dotenv.dart
+++ lib/dotenv.dart
@@ -1,20 +1,23 @@
 import 'dart:io';
 
 class DotEnv {
+  DotEnv() {
+    readEnv();
+  }
+
   final Map<String, dynamic> _env = {};
   dynamic operator [](String key) => _env[key];
 
-  readEnv() async {
+  readEnv() {
     final line = {};
     final file = File('./assets/env/teste.env');
-    final content = await file.readAsString();
+    final content = file.readAsStringSync();
     final linhas = line.entries.map((e) => "${e.key}=${e.value}").toList();
     final dadosEnv = linhas..addAll(content.split('\n'));
     _trataLinha(dadosEnv);
   }
 
   _trataLinha(Iterable<String> dados) {
-    Map<String, dynamic> map = {};
     for (var x in dados) {
       var key = x.split('=')[0];
       var value = x.split('=')[1].split('#')[0];
