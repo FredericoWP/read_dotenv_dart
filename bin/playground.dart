31332b3495ea15e649e5f3d461374fb2e5771cf5
FredericoWP <frederico.wallace@yahoo.com>
2022-11-19 13:57:24 -0300

    Tipando os values

--- bin/playground.dart
+++ bin/playground.dart
@@ -2,8 +2,10 @@ import 'package:playground/dotenv.dart';
 
 void main() async {
   DotEnv dotEnv = DotEnv();
-  print(dotEnv['DATABASE_URL']);
-  print(dotEnv['SWAGGER']);
-  print(dotEnv['JWT_ACCESS_TOKEN_EXPIRESIN']);
-  print(dotEnv['JWT_REFRESH_TOKEN_EXPIRESIN']);
+  print("${dotEnv['DATABASE_URL'].runtimeType} => ${dotEnv['DATABASE_URL']} ");
+  print("${dotEnv['SWAGGER'].runtimeType} => ${dotEnv['SWAGGER']} ");
+  print(
+      "${dotEnv['JWT_ACCESS_TOKEN_EXPIRESIN'].runtimeType} => ${dotEnv['JWT_ACCESS_TOKEN_EXPIRESIN']} ");
+  print(
+      "${dotEnv['JWT_REFRESH_TOKEN_EXPIRESIN'].runtimeType} => ${dotEnv['JWT_REFRESH_TOKEN_EXPIRESIN']} ");
 }
--- lib/dotenv.dart
+++ lib/dotenv.dart
@@ -20,9 +20,18 @@ class DotEnv {
   _trataLinha(Iterable<String> dados) {
     for (var x in dados) {
       var key = x.split('=')[0];
-      var value = x.split('=')[1].split('#')[0];
-      final map = <String, Object?>{key: value.trim()};
+      dynamic value = x.replaceAll("$key=", '').split('#')[0];
+      value = _typeConvert(value.trim());
+      final map = <String, Object?>{key: value};
       _env.addEntries(map.entries);
     }
+  }
+
+  _typeConvert(String value) {
+    if (int.tryParse(value) != null) return int.tryParse(value);
+    if (DateTime.tryParse(value) != null) return DateTime.tryParse(value);
+    if (value == "true") return true;
+    if (value == "false") return false;
+    return value;
   }
 }
